9c7820b80b78ed5027a10de1a6e22f4f600a09c5
Zatelbeeit <breaul@gmail.com>
2019-10-08 12:31:14 +0200

    ze canard is dead vive le canard

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
-function render () {
-    
-    var boardElement = document.body;
-    var board = document.createElement("section");
-    
-    var target = document.createElement("div");
 
-    board.classList.add("board");
+function frame () {
+    var newx = Math.random() * 450
+    var newy = Math.random() * 450
+
+    target.style.top = newy + "px"
+    target.style.left = newx + "px"    
+    target.style.transitionDuration = "1s"   
+}
+ 
+    
+var boardElement = document.body;
+var board = document.createElement("section");
+
+var target = document.createElement("div");
+
+board.classList.add("board");
+target.classList.add("target")
+
+boardElement.appendChild(target)
+document.body.appendChild(board)
+
+var frameInterval = setInterval(frame , 1000);
+
+
+function killDuck(event){
+
+    boardElement.removeChild(this)
+    target = document.createElement("div");
+    boardElement.appendChild(target); 
     target.classList.add("target")
-
-    boardElement.appendChild(target)
-    document.body.appendChild(board)
-
-    var position = 0;
-    var positionX = 0;
-    var positionY = 0;
-    var frameInterval = setInterval(frame , 10);
-
-    function frame () {
-        position++;
-        if (position +50 < 500) {
-           target.style.top = positionX + "px"
-           target.style.left = positionY + "px"       
-        }
-            // if (position > 450) {
-            //     target.style.bottom = position + "px"
-            //     target.style.right = position + "px"
-            // }
-    }
+    
+    var newx = Math.random() * 450
+    var newy = Math.random() * 450
+    target.style.top = newy + "px"
+    target.style.left = newx + "px"    
+    target.style.transitionDuration = "1s" 
+     
+    target.addEventListener("click", killDuck);
 
 }
 
-render();
+target.addEventListener("click", killDuck);
+
